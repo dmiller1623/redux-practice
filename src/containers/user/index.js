6b350643e2a6756422b7526c9f68d00718d8e98f
Dennis <dmiller1623@gmail.com>
2019-01-09 15:29:50 -0700

    Add redux setup

---
+++ src/containers/user/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+
+class User extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+ 
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input
+            type='text'
+            name='userName'
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.userName}
+          />
+          <input
+            type='text'
+            name='email'
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.email}
+          />
+          <input
+            type='text'
+            name='password'
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.password}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import './index.css';
-import App from './App';
+import App from './components/app/App.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
